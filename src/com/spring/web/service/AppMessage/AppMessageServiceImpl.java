1816ee7b9ee8ec903f4c7b09fbff374b16062a68
jiangtao <1075335720@qq.com>
2019-05-05 13:52:56 +0800

    5月5号

--- src/com/spring/web/service/AppMessage/AppMessageServiceImpl.java
+++ src/com/spring/web/service/AppMessage/AppMessageServiceImpl.java
@@ -1,11 +1,13 @@
 package com.spring.web.service.AppMessage;
 
 import com.spring.web.dao.AppControllerMessageMapper;
-import com.spring.web.model.Messages;
+import com.spring.web.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+
 @Service("AppMessageService")
 public class AppMessageServiceImpl implements AppMessageService {
 
@@ -13,7 +15,39 @@ public class AppMessageServiceImpl implements AppMessageService {
     private AppControllerMessageMapper appControllerMessageMapper;
 
     @Override
-    public List<Messages> findMessageList(String userId) {
-        return  appControllerMessageMapper.findMessageList(userId);
+    public List<Messages> findMessageList(String userId,Integer pageNo,Integer pageSize) {
+        return  appControllerMessageMapper.findMessageList(userId,pageNo,pageSize);
     }
+
+    @Override
+    public Messages findMessageDetail(String id) {
+        return appControllerMessageMapper.findMessageDetail(id);
+    }
+
+    @Override
+    public List<TCheck> findTCheckList(String cid,Integer pageNo,Integer pageSize) {
+        return appControllerMessageMapper.findTCheckList(cid,pageNo,pageSize);
+    }
+
+    @Override
+    public TCheck findCheckById(String checkId) {
+        return appControllerMessageMapper.findCheckById(checkId);
+    }
+
+    @Override
+    public ZzjgPersonnel findPersonnel(String userId) {
+        return appControllerMessageMapper.findPersonnel(userId);
+    }
+
+    @Override
+    public List<Map> findRectificationList(String checkId) {
+        return appControllerMessageMapper.findRectificationList(checkId);
+    }
+
+    @Override
+    public TRectification findTRectification(String checkId) {
+        return appControllerMessageMapper.findTRectification(checkId);
+    }
+
+
 }

d61906234d0023d65d67bd7a911fe2aca3287591
Administrator <2997922446@qq.com>
2019-05-17 09:51:31 +0800

    1111

---
+++ src/com/spring/web/model/request/Msg.java
@@ -0,0 +1,64 @@
+package com.spring.web.model.request;
+
+/**
+ * @Author: 桃红梨白
+ * @Date: 2019/05/16 11:04
+ * 短信通知类
+ */
+public class Msg {
+    private int code;
+    private String msg;
+    private Object body;
+
+    public Msg() {
+        this.code = 101;
+        this.msg = "系统未知错误！";
+    }
+
+    public Msg(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Msg(int code, String msg, Object body) {
+        this.code = code;
+        this.msg = msg;
+        this.body = body;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "Msg{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", body=" + body +
+                '}';
+    }
+
+
+
+}

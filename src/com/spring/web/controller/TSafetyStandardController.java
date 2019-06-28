243912e302569bd2e75668c16aacb0a6a42c5b08
桃红梨白 <2997922446@qq.com>
2019-06-28 10:21:42 +0800

    1

--- src/com/spring/web/controller/TSafetyStandardController.java
+++ src/com/spring/web/controller/TSafetyStandardController.java
@@ -43,64 +43,56 @@ public class TSafetyStandardController extends BaseController {
      * @return
      */
     @RequestMapping(value = "/findAll")
-    public String findAll(Integer parendId, Integer flag, HttpServletRequest request, Model model,Integer  sort) {
+    public String findAll(Integer parendId, Integer flag, HttpServletRequest request, Model model, Integer sort) {
         User user = getLoginUser(request);
-        if(null==sort){
-            sort=1;
+        if (null == sort) {
+            sort = 1;
         }
         HashMap<String, Object> map = new HashMap<>();
 
-        map.put("userId",user.getId());
-        map.put("parentId",parendId);
-        map.put("flag",flag);
-        map.put("sort",sort);
+        map.put("userId", user.getId());
+        map.put("parentId", parendId);
+        map.put("flag", flag);
+        map.put("sort", sort);
 
         List<TSafetyStandard> TSafetyStandardlist = tSafetyStandardMapper.findAll(map);
 
         // 判断是否有顺序,有书序就按照顺序来,没有就是倒序
-        model.addAttribute("sort",sort);
-        model.addAttribute("list",TSafetyStandardlist);
+        model.addAttribute("sort", sort);
+        model.addAttribute("list", TSafetyStandardlist);
         return "company/tables/tab-biaozhun2";
 
     }
 
     /**
-     *  根据A级元素id查询B级元素
+     * 根据A级元素id查询B级元素
+     *
      * @return
      */
-    @RequestMapping(value="/findByParentId")
-    public String findByParentId(Integer safetyStandardlistId,Model model,Integer sort){
-        if(null==sort){
-            sort=1;
+    @RequestMapping(value = "/findByParentId")
+    public String findByParentId(Integer safetyStandardlistId, Model model, Integer sort) {
+        if (null == sort) {
+            sort = 1;
         }
-        List<TSafetyStandard> TSafetyStandard = tSafetyStandardMapper.findByparentId(safetyStandardlistId,sort);
-        model.addAttribute("list",TSafetyStandard);
-        model.addAttribute("parentId",safetyStandardlistId);
-        model.addAttribute("sort",sort);
+        List<TSafetyStandard> TSafetyStandard = tSafetyStandardMapper.findByparentId(safetyStandardlistId, sort);
+        model.addAttribute("list", TSafetyStandard);
+        model.addAttribute("parentId", safetyStandardlistId);
+        model.addAttribute("sort", sort);
         return "company/tables/tab-biaozhunB";
     }
 
     /**
      * 根据 id查询项目信息 并调转动修改的页面
+     *  普通的跳转页面,没有请求
      * @param safetyStandardlistId
      * @return
      */
-    @RequestMapping(value = "/findOne",method=RequestMethod.POST)
-    @ResponseBody
-    public Result findOne(Integer safetyStandardlistId) {
-        Result  result =new ResultImpl();
-        try {
-            TSafetyStandard TSafetyStandard = tSafetyStandardMapper.findOne(safetyStandardlistId);
-            result.setMess("查询成功");
-            result.setObject(TSafetyStandard);
-            result.setStatus("0");
-            return result;
-        } catch (Exception e) {
-            e.printStackTrace();
-            result.setStatus("1");
-            result.setMess("未找到数据");
-            return result;
-        }
+    @RequestMapping(value = "/findOne")
+    public String findOne(Integer safetyStandardlistId,Model model) {
+
+        TSafetyStandard TSafetyStandard = tSafetyStandardMapper.findOne(safetyStandardlistId);
+        model.addAttribute("item",TSafetyStandard);
+        return "company/tables/tab-detail";
     }
 
     /**
@@ -111,22 +103,22 @@ public class TSafetyStandardController extends BaseController {
      */
     @RequestMapping(value = "/save")
     public @ResponseBody
-    Result save(@RequestBody TSafetyStandard tSafetyStandard,HttpServletRequest request) {
+    Result save(@RequestBody TSafetyStandard tSafetyStandard, HttpServletRequest request) {
         Result result = new ResultImpl();
         User loginUser = getLoginUser(request);
         Company company = companyMapper.selectByPrimaryKey(loginUser.getId());
-        try{
+        try {
 
             String industry = company.getIndustry();
-            if(industry.indexOf("化工")!=-1){
+            if (industry.indexOf("化工") != -1) {
                 tSafetyStandard.setIndustryId(1);
-            }else{
+            } else {
                 tSafetyStandard.setIndustryId(2);
             }
             tSafetyStandard.setUserId(loginUser.getId());
-            if(tSafetyStandard.getParentId()==0){
+            if (tSafetyStandard.getParentId() == 0) {
                 tSafetyStandard.setFlag(1);
-            }else{
+            } else {
                 tSafetyStandard.setFlag(2);
             }
 
@@ -137,7 +129,7 @@ public class TSafetyStandardController extends BaseController {
             result.setStatus("0");
             result.setObject(tSafetyStandard.getId());
             return result;
-        }catch (Exception e){
+        } catch (Exception e) {
             result.setMess("保存失败");
             result.setStatus("1");
             result.setObject(tSafetyStandard.getId());
@@ -148,13 +140,14 @@ public class TSafetyStandardController extends BaseController {
 
     /**
      * 修改数据
+     *
      * @param tSafetyStandard
      * @return
      */
-    @RequestMapping(value="/update-tSafetyStandard",method = RequestMethod.POST)
+    @RequestMapping(value = "/update-tSafetyStandard", method = RequestMethod.POST)
     public @ResponseBody
-    Result updateTSafetyStandard(@RequestBody TSafetyStandard tSafetyStandard){
-        Result result =new ResultImpl();
+    Result updateTSafetyStandard(@RequestBody TSafetyStandard tSafetyStandard) {
+        Result result = new ResultImpl();
 
         try {
             tSafetyStandardMapper.updateTSafetyStandard(tSafetyStandard);
@@ -171,16 +164,17 @@ public class TSafetyStandardController extends BaseController {
 
     /**
      * 删除要素,要判断是否有这个要素点,第二这个要素点的子id有没有数据,有的话让其将子要素删除,然后才能删除
+     *
      * @param safetyStandardlistId
      * @return
      */
-    @RequestMapping(value="/delete-tSafetyStandard",method = RequestMethod.POST)
+    @RequestMapping(value = "/delete-tSafetyStandard", method = RequestMethod.POST)
     @ResponseBody
-    public Result deleteTSafetyStandard(Integer safetyStandardlistId ){
+    public Result deleteTSafetyStandard(Integer safetyStandardlistId) {
         Result result = new ResultImpl();
         try {
-            List<TSafetyStandard> TSafetyStandard = tSafetyStandardMapper.findByparentId(safetyStandardlistId,null);
-            if(null!=TSafetyStandard&&TSafetyStandard.size()>0){
+            List<TSafetyStandard> TSafetyStandard = tSafetyStandardMapper.findByparentId(safetyStandardlistId, null);
+            if (null != TSafetyStandard && TSafetyStandard.size() > 0) {
                 result.setStatus("1");
                 result.setMess("该数据存在下级要素点,请删除完后在进行删除");
                 return result;
@@ -200,31 +194,30 @@ public class TSafetyStandardController extends BaseController {
     /**
      * 自动导入功能
      * 首先判断这个公司类型
-     *
+     * <p>
      * 先根据名称进行判断,是否包含化工企业,
      * 两种企业, 工贸/化工
-     *
      *
      * @return
      */
     @RequestMapping("/Automatic-import")
     @SuppressWarnings("all")
     @ResponseBody
-    public Result automaticImport(HttpServletRequest request){
+    public Result automaticImport(HttpServletRequest request) {
         Result result = new ResultImpl();
         try {
             User user = getLoginUser(request);// 获取公司名称
             Company company = companyMapper.selectByPrimaryKey(user.getId());
 
             String industry = company.getIndustry();
-            Integer industryType =null;
-            if(industry.indexOf("化工")!=-1){
-                industryType=1; // 危化企业
-            }else{
-                industryType=2; // 工贸企业
+            Integer industryType = null;
+            if (industry.indexOf("化工") != -1) {
+                industryType = 1; // 危化企业
+            } else {
+                industryType = 2; // 工贸企业
             }
             // 1. 查询关于该行业的A级要素
-            List<TSafety> tSafetyList= tSafetyMapper.selectAByIndustryType(industryType,0);
+            List<TSafety> tSafetyList = tSafetyMapper.selectAByIndustryType(industryType, 0);
             for (TSafety tSafety : tSafetyList) {
                 //先插入A级要素
                 TSafetyStandard tSafetyStandard = new TSafetyStandard();
@@ -263,11 +256,10 @@ public class TSafetyStandardController extends BaseController {
 
     /**
      * 自动导入文本文件数据进行保存
-     *
      */
     @RequestMapping("/import-table")
     @ResponseBody
-    public Result importTable(@RequestBody  TSafety tSafety){
+    public Result importTable(@RequestBody TSafety tSafety) {
         Result result = new ResultImpl();
         tSafetyMapper.insertTSafety(tSafety);
         //判断该公司类型

b66819fb8b9dd46c80a167d850748c33d8f5cd4c
HaveFuner <13706174234@163.com>
2019-06-28 15:25:40 +0800

    2019.6.28  13.55

---
+++ src/com/spring/web/dao/HiddenPlanMapper.java
@@ -0,0 +1,14 @@
+package com.spring.web.dao;
+
+import com.spring.web.model.ACompanyManual;
+import com.spring.web.model.request.CheckLevel;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+public interface HiddenPlanMapper {
+
+
+}
+
